ee5b9bf95a528ba607d27abeffe87fa2e77c4096
Your Name <you@example.com>
2022-05-12 19:25:53 +0200

    starting menu

---
+++ src/Menus/Menu.java
@@ -0,0 +1,24 @@
+package Menus;
+
+public class Menu {
+
+    public boolean running;
+    protected String title;
+
+    public Menu() {
+        running = true;
+        this.title = title;
+
+
+        public void start() {
+            while (running) {
+                // imprimir titulo del menu
+                System.out.println(title);
+                // imprimir las opciones
+
+                // pedir al usuario la opcion que quiere
+                // ejecutar la opcion elejida
+            }
+        }
+    }
+}
